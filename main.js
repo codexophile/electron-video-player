4fe544aa6f90bc074113935ff93e5cec67453988
Codexophile <jaliya.chirantha@gmail.com>
2026-06-28 20:20:53 +0530

    - remove titlebar, borders. - transparet window. - .main-container -> drag

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
