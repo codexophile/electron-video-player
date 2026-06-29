72ae2866d10aabfe272c64fb26c58a8bf89ac113
Codexophile <jaliya.chirantha@gmail.com>
2026-06-29 13:38:35 +0530

    refactor: remove tutorial code

--- preload.js
+++ preload.js
@@ -1,10 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
   ping: () => ipcRenderer.invoke('ping'),
   getArgs: () => ipcRenderer.invoke('get-cli-args'),
-  // we can also expose variables, not just functions
 });
